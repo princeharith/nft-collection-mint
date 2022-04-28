635f9b0a83302fa68d60ade03a1ebd5d1976bc65
princeharith <reddy.harith9@gmail.com>
2022-04-28 00:26:16 -0400

    fixing api issue

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x1006cc50d1BB48d420f219b39350619FcF77e1b4";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-mint-test.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-mint-test.vercel.app/api/tokenId";
 
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
